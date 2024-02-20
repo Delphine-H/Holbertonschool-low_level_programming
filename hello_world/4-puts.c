ca1973f30e2d83b3d827841e46c06cb500e9d92b
Delphine HANNON <hannon.delphine@gmail.com>
2024-02-20 09:09:04 +0100

    cript that compiles a C file and creates an executable named cisfun.
        The C file name will be saved in the variable

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-    char text[] = "\"Programming is like building a multilingual puzzle";
-    puts(text);
-    return (0);
+	char text[] = "\"Programming is like building a multilingual puzzle";
+   
+	puts(text);
+	return (0);
 }
