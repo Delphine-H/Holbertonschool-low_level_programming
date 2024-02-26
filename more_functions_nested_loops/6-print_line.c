ddad4a1f7a9ba9532a639f2a280c3e3ebf8a2c93
Delphine HANNON <hannon.delphine@gmail.com>
2024-02-26 12:11:30 +0100

    Write a function that draws a straight line in the terminal.
        Prototype: void print_line(int n);
        You can only use _putchar function to print
        Where n is the number of times the character _ should be printed
        The line should end with a \n
        If n is 0 or less, the function should only print \n

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straignt line in the therminal
+ * followed by a new line.
+ *
+ *
+ */
+
+void print_line(int n)
+{
+	int i;
+
+		for (i = 0; i <= n; i++)
+		{
+			putchar('_');
+		}
+	_putchar('\n');
+	}
+}
