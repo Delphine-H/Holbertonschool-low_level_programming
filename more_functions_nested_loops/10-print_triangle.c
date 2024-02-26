1c0a94ccf583f976b34139752161b1079f02031c
Delphine HANNON <hannon.delphine@gmail.com>
2024-02-26 16:53:30 +0100

    Write a function that prints a triangle, followed by a new line.
        Prototype: void print_triangle(int size);
        You can only use _putchar function to print
        Where size is the size of the triangle
        If size is 0 or less, the function should print only a new line
        Use the character # to print the triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -3,7 +3,7 @@
 /**
  * print_triangle - function that prints a triangle
  * followed by a new line.
- *
+ *@size: size of triangle
  *
  *Return: (0) sucess
  */
@@ -16,12 +16,12 @@ void print_triangle(int size)
 	{
 		for (i = size; i > 0; i--)
 		{
-			for (ii = 1; ii < i; ii++) 
+			for (ii = 1; ii < i; ii++)
 			{
 				_putchar(' ');
 			}
-			for (j = 0; j <= (size -i); j++)
-			{	
+			for (j = 0; j <= (size - i); j++)
+			{
 			_putchar('#');
 			}
 		_putchar('\n');
