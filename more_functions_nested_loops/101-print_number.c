cbefa461f068d9049348b4b3cee9b3eb71545f78
Delphine HANNON <hannon.delphine@gmail.com>
2024-03-09 20:55:43 +0100

    Write a function that prints an integer.
        Prototype: void print_number(int n);
        You can only use _putchar function to print
        You are not allowed to use long
        You are not allowed to use arrays or pointers
        You are not allowed to hard-code special values

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ *@n: number to print
+ *
+ *Return: nothing
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+	recursion_lastdigit(n);
+	}
+}
+
+
+
+/**
+ * recursion_lastdigit - function that prints lastdigit
+ *@n: number to print
+ *
+ *Return: nothing
+ */
+
+void recursion_lastdigit(int n)
+{
+	int lastdigit;
+
+	if (n != 0)
+	{
+	lastdigit = n % 10;
+	n = n / 10;
+	recursion_lastdigit(n);
+	_putchar(lastdigit + '0');
+	}
+}
