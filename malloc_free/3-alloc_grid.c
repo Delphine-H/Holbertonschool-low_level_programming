f19333ed20ff64fe7120944eeb549e3ecb779f85
Delphine-H <hannon.delphine@gmail.com>
2024-03-12 20:09:38 +0100

    Write a function that returns a pointer to a 2 dimensional array of integers.
        Prototype: int **alloc_grid(int width, int height);
        Each element of the grid should be initialized to 0
        The function should return NULL on failure
        If width or height is 0 or negative, return NULL

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -12,20 +12,26 @@
 int **alloc_grid(int width, int height)
 {
 	int **grid;
+	int i;
 	int row, column;
 
 	if (width != 0 && height != 0)
 	{
-		grid = malloc(height * sizeof(int));
+		grid = malloc(height * sizeof(int *));
 		if (grid == NULL)
 		{
 			return (NULL);
 		}
 		for (row = 0; row < height; row++)
 		{
-			grid[row] = malloc(width* sizeof(int));
+			grid[row] = malloc(width * sizeof(int));
 			if (grid[row] == NULL)
 			{
+				for (int i = 0, i < row; i++)
+				{
+					free(grid[i]);
+				}
+				free(grid);
 				return (NULL);
 			}
 			for (column = 0; column < width; column++)
