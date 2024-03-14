a5e1e200aea0a0ee85fb8aaff691e6332be41384
Delphine-H <hannon.delphine@gmail.com>
2024-03-14 17:22:02 +0100

    Define a new type struct dog with the following elements:
        name, type = char *
        age, type = float
        owner, type = char *

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,7 +2,12 @@
 #define MAIN
 
 /**
- *dog - structure of dog
+ *struct dog - structure of dog
+ *@name: name of dog
+ *@age: age of the dog
+ *@owner: dog's owner name
+ *
+ *Description: datas of a dog name age owner patati patata...
 */
 typedef struct dog
 {
