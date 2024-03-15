511d13c00bd748ba17b82bf74f9d3148b4ec5113
Delphine-H <hannon.delphine@gmail.com>
2024-03-15 11:55:34 +0100

    Write a function that initialize a variable of type struct dog
        Prototype: void init_dog(struct dog *d, char *name, float age, char *owner);

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * init_dog - function that initialize a variable of type
@@ -9,8 +10,18 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-	
+	if (name != NULL)
+	{
+		d->name = name;
+	}
+	if (age >= 0)
+	{
+		d->age = age;
+	}
+	if (owner != NULL)
+	{
+		d->owner = owner;
+	}
+
+	printf("Ok");
 }
