ebbb86353d2fbb52e2d15413e119ea9e6312b4a3
Delphine-H <hannon.delphine@gmail.com>
2024-03-18 12:56:03 +0100

    Write a function that executes a function given as a parameter on each element of an array.
        Prototype: void array_iterator(int *array, size_t size, void (*action)(int));
        where size is the size of the array
        and action is a pointer to the function you need to use

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,8 +1,11 @@
 #ifndef MAIN
 #define MAIN
 
+#include <stddef.h>
+
 void print_name_uppercase(char *name);
 void print_name_as_is(char *name);
 void print_name(char *name, void(*f)(char*));
+void array_iterator(int *array, size_t size, void(*action)(int));
 
 #endif
