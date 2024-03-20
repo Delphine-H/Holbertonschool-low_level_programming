b8c4d804796d5d74ca2dab37b8b4941ca2680163
Delphine-H <hannon.delphine@gmail.com>
2024-03-20 09:11:06 +0100

    Write a function that prints a name.
        Prototype: void print_name(char *name, void (*f)(char *));

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,8 +3,6 @@
 
 #include <stddef.h>
 
-void print_name_uppercase(char *name);
-void print_name_as_is(char *name);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void(*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
