8f32c12fcfc6b222fcd9ae53a313ed2d961312b1
Delphine-H <hannon.delphine@gmail.com>
2024-03-21 01:37:27 +0100

    Write a function that prints anything.
        Prototype: void print_all(const char * const format, ...);
        where format is a list of types of arguments passed to the function
            c: char
            i: integer
            f: float
            s: char * (if the string is NULL, print (nil) instead
            any other char should be ignored
            see example
        You are not allowed to use for, goto, ternary operator, else, do ... while
        You can use a maximum of
            2 while loops
            2 if
        You can declare a maximum of 9 variables
        You are allowed to use printf
        Print a new line at the end of your function

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -6,7 +6,7 @@
 /**
  * print_all - function that prints anything
  * @format: list of arguments passed to the function
- * 
+ *
  * Return: nothing
 */
 
@@ -18,30 +18,30 @@ void print_all(const char * const format, ...)
 	char *str;
 
 	va_start(args, format);
-	
+
 	while (format && format[i])
 	{
 		c = format[i];
 		switch (c)
 		{
-            case 'c':
-            	printf("%c", va_arg(args, int));
-                break;
-            case 'i':
-                printf("%d", va_arg(args, int));
-                break;
-            case 'f':
-                printf("%f", (float)va_arg(args, double));
-                break;
-            case 's':
-                str = va_arg(args, char *);
-                if (str == NULL)
-                    str = "(nil)";
-                printf("%s", str);
-                break;
-   		 }
-		 if ((c == 'c' || c == 'i' || c == 'f' || c == 's') && format[i + 1] != '\0')
-		 	printf(", ");
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", (float)va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+		}
+		if ((c == 'c' || c == 'i' || c == 'f' || c == 's') && format[i + 1] != '\0')
+			printf(", ");
 		i++;
 	}
 
