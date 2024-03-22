813df83e99f08b9dcc23eb1b18ff916801a73344
Delphine-H <hannon.delphine@gmail.com>
2024-03-22 15:02:03 +0100

    erreur d'opérateur

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	{
 		operation = get_op_func(argv[2]);
 
-		if (operation != NULL)
+		if (operation != NULL && argv[2][1] == '\0')
 		{
 			op = *(argv[2]);
 			if ((op == '/' || op == '%') && atoi(argv[3]) == 0)
