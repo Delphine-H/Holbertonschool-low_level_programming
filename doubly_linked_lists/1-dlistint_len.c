d31bcd12283fc47175c58d8cff4a0b69f7d704af
Delphine-H <hannon.delphine@gmail.com>
2024-04-03 16:07:13 +0200

    add function that returns the number of elements in a dobble linked list

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - funtion that adds a new node at the beginning of a dobble
+ * linked list
+ * @h : dobble linked list of integer
+ * return: number of elements
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	int nbElements;
+
+	while (h && h->prev)
+		h = h->prev;
+
+	while (h)
+	{
+		nbElements++;
+		h = h->next;
+	}
+
+	return (nbElements);
+}
