0ce7aa0b6ca6bc222bc033a50dd60e9e6c8cec1b
Delphine-H <hannon.delphine@gmail.com>
2024-04-03 15:26:45 +0200

    initialise nbNode to 0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,19 +3,19 @@
 /**
  * print_dlistint - function that prints all the elements of
  * a dlistin_t list
- * @dlistint_t: list of int elements to print
+ * @h: list of int elements to print
  * Return: number of nodes
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nbNode;
+	size_t nbNode = 0;
 
 	if (!h)
 		return (0);
 
-	while (h)
+	while (h != NULL)
 	{
-		printf("d\n", h->n);
+		printf("%d\n", h->n);
 		nbNode++;
 		h = h->next;
 	}
