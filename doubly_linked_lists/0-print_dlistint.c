8d93254b27c1a9bd43074945da003fa4e727eae5
Delphine-H <hannon.delphine@gmail.com>
2024-04-03 15:47:40 +0200

    add loop to return to the first node

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -9,11 +9,15 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nbNode = 0;
+	
 
 	if (!h)
 		return (0);
 
-	while (h != NULL)
+	while (h && h->prev)
+		h = h->prev;
+
+	while (h)
 	{
 		printf("%d\n", h->n);
 		nbNode++;
