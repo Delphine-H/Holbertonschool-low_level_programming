b94f661c5ffd7205ddcddb480adb8e35fbd02caa
Delphine-H <hannon.delphine@gmail.com>
2024-04-04 21:58:44 +0200

    add function that adds a new node at the end of a dobble linked list

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	newData = malloc(sizeof(dlistint_t));
 	if (newData == NULL)
 		return (NULL);
-	
+
 	if (*head == NULL)
 	{
 		newData->prev = NULL;
