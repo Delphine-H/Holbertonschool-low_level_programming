dfce1667f0f72fa15297251a758fe736596b3044
Delphine-H <hannon.delphine@gmail.com>
2024-04-04 15:52:46 +0200

    initialize lengthString to 0

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,7 +11,7 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *newData;
 	list_t *lastNode;
-	int lengthString;
+	int lengthString = 0;
 
 	newData = malloc(sizeof(list_t));
 	if (newData == NULL)
@@ -43,5 +43,4 @@ list_t *add_node_end(list_t **head, const char *str)
 	lastNode->next = newData;
 
 	return (newData);
-
 }
