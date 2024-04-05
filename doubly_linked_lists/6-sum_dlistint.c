21b0de2f714f9ca7293759a61701baa4b5eb0f25
Delphine-H <hannon.delphine@gmail.com>
2024-04-05 10:42:35 +0200

    add function that returns the sum of all the data of a dobble linked list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - function that returns the sum of all the data (n)
+ * of a dobble linked list
+ * @head: pointer to the head node of the list
+ * Return: sum of all the data
+*/
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head->next)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
