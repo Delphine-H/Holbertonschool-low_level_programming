2055991b6736c65714aacdc41b682099f925a8c8
Delphine-H <hannon.delphine@gmail.com>
2024-04-06 17:43:21 +0200

    delete trailing whitespace

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	/* Check if filename is NULL */
 	if (filename == NULL)
 		return (-1);
-	
+
 	if (text_content == NULL)
 		return (1);
 
