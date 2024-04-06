8abc43adfbbf4975359528610a2111171b9e5846
Delphine-H <hannon.delphine@gmail.com>
2024-04-06 20:37:55 +0200

    add a error in case bytes_read == -1

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -22,7 +22,6 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd_from = open(argv[1], O_RDONLY); /* Open the source file */
 	if (fd_from == -1)
 	{
@@ -33,7 +32,11 @@ int main(int argc, char *argv[])
 	/*Open or create the destination file, with appropriate permission */
 	fd_to = open(argv[2], O_WRONLY | O_CREAT |
 	O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 	while ((bytes_read = read(fd_from, buffer, 1024)) > 0) /* Copy content */
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
@@ -43,9 +46,12 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	close_file_description(fd_from, fd_to); /*close the file descriptor*/
-
 	return (0);
 }
 
